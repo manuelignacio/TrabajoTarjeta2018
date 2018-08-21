688df73bc8afa828da1e42a0186e74a1fac5a505
Tadeo Schlieper <tadeo.sch@hotmail.com>
2018-08-21 18:23:45 -0300

    Agregadas funcionalidades de plus y pagar en Tarjeta

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -3,17 +3,18 @@
 namespace TrabajoTarjeta;
 
 class Tarjeta implements TarjetaInterface {
-    protected $saldo;
+    protected $saldo = 0; // float
+    protected $plus = 0; // int
 
     public function recargar($monto) {
-      // Esto esta hecho mal a proposito.
       if ($monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100 || $monto == 510.15 || $monto == 962.59) {
+        $monto -= ($this->plus * 14.8); // el costo de los viajes plus siempre sera 14.8, independientemente de su franquicia
         $this->saldo += $monto;
+        $this->plus = 0;
         if ($monto == 510.15) $this->saldo += 81.93;
         if ($monto == 962.59) $this->saldo += 221.58;
         return true;
       }
-
       return false;
     }
 
@@ -24,6 +25,16 @@ class Tarjeta implements TarjetaInterface {
      */
     public function obtenerSaldo() {
       return $this->saldo;
+    }
+
+    public function pagar($valor) {
+      if ($this->saldo < 0) return false; // no se toleraran valores negativos
+      if ($this->saldo < $valor) {
+        if ($this->plus > 1) return false; // como maximo podra adeudar 2 viajes plus
+        $this->plus ++;
+      }
+      else $this->saldo -= $valor;
+      return true;
     }
 
 }
