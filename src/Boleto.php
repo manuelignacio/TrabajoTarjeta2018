52b0829a6ced321eb656551f0865525540d83989
Tadeo Schlieper <tadeo.sch@hotmail.com>
2018-09-24 02:42:35 -0300

    Añadidos metodos de obtener en Boleto

--- src/Boleto.php
+++ src/Boleto.php
@@ -9,9 +9,9 @@ class Boleto implements BoletoInterface {
     protected $tarjeta; // TarjetaInterface
     protected $fecha; // int
     protected $tarjetaTipo; // string
-    protected $totalAbonado; // float
-    protected $tarjetaSaldo; // float
     protected $tarjetaID; // int
+    protected $tarjetaSaldo; // float
+    protected $totalAbonado; // float
     protected $descripcion; // string
 
     public function __construct($valor, $colectivo, $tarjeta) {
@@ -26,9 +26,11 @@ class Boleto implements BoletoInterface {
         $plusAbonados = $tarjeta->obtenerPlusDevueltos() * $tarjeta->obtenerValor();
         $this->totalAbonado = (int)(!$tarjeta->obtenerUsoPlus()) * ($valor + $plusAbonados);
         $this->descripcion = "";
-        if ($tarjeta->obtenerUsoPlus()) $this->descripcion = "Viaje Plus \$0.00\n";
+        $this->descripcion .= "Linea: {$colectivo->linea()}\n";
+        $this->descripcion .= date("d/m/Y H:i:s", $this->fecha) . "\n";
+        if ($tarjeta->obtenerUsoPlus()) $this->descripcion .= "Viaje Plus \$0.00\n";
         else {
-            if ($tarjeta->obtenerPlusDevueltos() > 0) $this->descripcion = "Abona {$tarjeta->obtenerPlusDevueltos()} Viajes Plus {$plusAbonados} y\n";
+            if ($tarjeta->obtenerPlusDevueltos() > 0) $this->descripcion .= "Abona {$tarjeta->obtenerPlusDevueltos()} Viajes Plus {$plusAbonados} y\n";
             $this->descripcion .= "Tarros: {$valor}\nTotal abonado: \${$this->totalAbonado}\n";
         }
         $this->descripcion .= "Saldo(S.E.U.O): \${$this->tarjetaSaldo}";
@@ -44,6 +46,30 @@ class Boleto implements BoletoInterface {
 
     public function obtenerTarjeta() {
         return $this->tarjeta;
+    }
+
+    public function obtenerFecha() {
+        return $this->fecha;
+    }
+
+    public function obtenerTarjetaTipo() {
+        return $this->tarjetaTipo;
+    }
+
+    public function obtenerTarjetaID() {
+        return $this->tarjetaID;
+    }
+
+    public function obtenerTarjetaSaldo() {
+        return $this->tarjetaSaldo;
+    }
+
+    public function obtenerTotalAbonado() {
+        return $this->totalAbonado;
+    }
+
+    public function obtenerDescripcion() {
+        return $this->descripcion;
     }
 
 }
